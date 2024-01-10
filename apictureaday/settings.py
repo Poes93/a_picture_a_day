afc9b0b9c0167c9323803347cd0732e355ae54ea
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2024-01-10 11:38:13 +0100

    Heroku deply test

--- apictureaday/settings.py
+++ apictureaday/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s$jpmoes7d^wsw%e#4ff%x&lneh-!9bxa2ub1nn-ls5&4(@5e='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
 
 
 # Application definition
