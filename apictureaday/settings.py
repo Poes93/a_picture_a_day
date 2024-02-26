53e6b4449a108c681d9f488612a8ea333cf98f9b
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2024-02-26 13:48:54 +0100

    Update settings.py

--- apictureaday/settings.py
+++ apictureaday/settings.py
@@ -158,6 +158,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 
