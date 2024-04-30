ad678807ed2f1ebe39013e3ebc60cc84bef5c8e8
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2024-04-30 17:48:53 +0200

    add login required to specific pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,15 @@ from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from .models import Post, Comment
 from .forms import CommentForm, PostForm
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.decorators import method_decorator
 
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "index.html"
     paginate_by = 8
-
 
 class PostDetail(View):
 
@@ -36,6 +37,7 @@ class PostDetail(View):
             }
         )
 
+    @method_decorator(login_required)
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -48,8 +50,8 @@ class PostDetail(View):
 
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
-            new_comment.author = request.user  # Set the comment's author as the current user
-            new_comment.post = post  # Assuming you have the post object already
+            new_comment.author = request.user
+            new_comment.post = post
             new_comment.save()
 
         else:
@@ -67,9 +69,9 @@ class PostDetail(View):
             }
         )
 
-
 class PostLike(View):
 
+    @method_decorator(login_required)
     def post(self, request, slug, *args, **kwargs):
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
@@ -79,8 +81,7 @@ class PostLike(View):
 
         return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
-
-class UserPostsView(generic.ListView):
+class UserPostsView(LoginRequiredMixin, generic.ListView):
     model = Post
     template_name = "user_posts.html"
     paginate_by = 8
@@ -89,9 +90,7 @@ class UserPostsView(generic.ListView):
         queryset = Post.objects.filter(author=self.request.user, status=1).order_by("-created_on")
         return queryset
 
-
-# User can only post once every 24 hours
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = "post_create.html"
@@ -99,91 +98,59 @@ class PostCreate(CreateView):
     def form_valid(self, form):
         post = form.save(commit=False)
         post.author = self.request.user
-
-        # Check if the 'Save as Draft' checkbox is selected
         if 'save_draft' in self.request.POST:
-            post.status = 'draft'  # Set the status of the post to 'draft'
+            post.status = 'draft'
         else:
-            post.status = 'published'  # Set the status of the post to 'published'
-
+            post.status = 'published'
         post.save()
         return HttpResponseRedirect(post.get_absolute_url())
 
-
-class PostUpdate(generic.UpdateView):
+class PostUpdate(LoginRequiredMixin, generic.UpdateView):
     model = Post
     form_class = PostForm
     template_name = "post_edit.html"
 
     def form_valid(self, form):
-        # Get the post object
         post = form.save(commit=False)
-        # Save any modifications to the post
         post.save()
-
-        # Construct the URL for the post detail page
         post_detail_url = reverse('post_detail', kwargs={'slug': post.slug})
-
-        # Redirect the user to the post detail page
         return redirect(post_detail_url)
 
-
-class PostDelete(generic.DeleteView):
+class PostDelete(LoginRequiredMixin, generic.DeleteView):
     model = Post
     template_name = "post_delete.html"
     pk_url_kwarg = 'pk'
-    success_url = reverse_lazy('user_posts')  # Default success URL
+    success_url = reverse_lazy('user_posts')
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         success_url = self.get_success_url()
-
-        # Delete the object
         self.object.delete()
-
-        # Redirect the user to the previous page
         referer = request.META.get('HTTP_REFERER')
         if referer:
             return HttpResponseRedirect(referer)
-
         return HttpResponseRedirect(success_url)
 
-
-class CommentEdit(generic.UpdateView):
-    model = Comment
-    fields = ['body']
-    template_name = "comment_edit.html"
-    pk_url_kwarg = 'comment_pk'
-
-    def get_success_url(self):
-        # Redirect to the post detail
-        return reverse('post_detail', args=[self.object.post.slug])
-
-
+@login_required
 def edit_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
     if request.user != comment.author:
-        # Redirect or show error if the user is not the author of the comment
-        return redirect('post_detail', slug=comment.post.slug)  # Adjust redirection as needed
-
+        return redirect('post_detail', slug=comment.post.slug)
     if request.method == 'POST':
         form = CommentForm(request.POST, instance=comment)
         if form.is_valid():
             form.save()
-            return redirect('post_detail', slug=comment.post.slug)  # Adjust redirection as needed
+            return redirect('post_detail', slug=comment.post.slug)
     else:
         form = CommentForm(instance=comment)
-
     return render(request, 'comment_edit.html', {'form': form, 'comment': comment})
 
-
+@login_required
 def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
-    post_slug = comment.post.slug  # Remember the slug of the post to which the comment belongs
+    post_slug = comment.post.slug
     if request.user == comment.author:
         comment.delete()
-        # Redirect to the post detail page after deletion
         return redirect('post_detail', slug=post_slug)
     else:
-        # Add your error handling here (e.g., setting a message, redirecting elsewhere)
         return redirect('post_detail', slug=post_slug)
