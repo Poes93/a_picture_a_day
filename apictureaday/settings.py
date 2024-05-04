d84c964cb6083fe824acafc866b91000abc23da8
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2024-05-04 16:44:02 +0200

    update base.html with static link

--- apictureaday/settings.py
+++ apictureaday/settings.py
@@ -159,7 +159,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
