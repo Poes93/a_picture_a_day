bcd94e23f5cb08967049acb50cf8400c78d2e1a0
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2024-05-05 20:42:43 +0200

    test heroku deploy

--- apictureaday/settings.py
+++ apictureaday/settings.py
@@ -157,26 +157,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = 'static/'
+
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+MEDIA_URL = "/media/"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Cloudinary settings for media storage
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dwazku2zg',
     'API_KEY': '894331299656218',
-    'API_SECRET': '_2uRVmVhDsyeRY_5ng6LtpjbZiU'
+    'API_SECRET': '_2uRVmVhDsyeRY_5ng6LtpjbZiU',
 }
-
-# Setting Cloudinary as the default storage for media files
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
